7567ca122f12cce5ba613430d591c4ebc75e233f
janghomoon <sonyjangho@gmail.com>
2025-09-23 23:38:06 +0900

    Initial commit

---
+++ src/test/java/kr/co/jaegertestars/JaegerTestARsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jaegertestars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaegerTestARsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
